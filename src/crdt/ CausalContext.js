ed0fab94ca19e99ab41c19009ddba7c61dddf748
Fábio Sá <up202007658@edu.fe.up.pt>
2023-12-08 22:21:57 +0000

    test with add, remove

--- src/crdt/ CausalContext.js
+++ src/crdt/ CausalContext.js
@@ -1,24 +1,62 @@
-let system = {
-    s: [], // (element, cc)
-    c: [], // (cc) = (id, version)
+class Test {
+
+    constructor(id) {
+        this.id = id;
+        this.set = []; // (element, cc)
+        this.cc = [];  // (cc) = (id, version)
+    }
+
+    next() {
+        const indexes = this.cc
+        .filter(entry => entry[0] === this.id && typeof entry[1] === 'number')
+        .map(entry => entry[1]);
+        console.log(indexes)
+        const index = indexes.length ? Math.max(...indexes) : 0;
+        console.log(index)
+        return [this.id, index + 1];
+    }
+
+    add(element) {
+        const d = this.next(this.cc);
+        console.log(d)
+        this.set.push([element, d]);
+        this.cc.push(d);
+    }
+
+    remove(element) {
+        this.set = this.set.filter((node) => {
+            return node[0] !== element
+        });
+    }
+
+    elements() {
+        return this.set.map((node) => node[0]);
+    }
+
+    showElements() {
+        console.log(this.set);
+    }
+
+    showCC() {
+        console.log(this.cc);
+    }
+
+    merge(test) {
+
+    }
 }
 
-const node_id = "nodeA"
+const test = new Test('unique');
 
-function next(c) {
-    const index = Math.max(c.map((id, n) => {
-        return (id == node_id) ? n : -1;
-    })) || 0;
-    return [node_id, index + 1];
-}
+test.add("banana")
+test.add("maca")
 
-function add(element) {
-    const d = next(system.c);
-    system.s.push([element, d]);
-    system.c.push(d);
-}
+test.remove("banana")
+test.showElements();
+test.showCC();
 
-add("something")
-console.log(system.s)
-add("something222")
-console.log(system.s)
+console.log("\n")
+
+test.add("banana");
+test.showElements();
+test.showCC();
